15714b1f133e597dc52a64d30dc59bbfe67356be
danbell123 <d.bell13@newcastle.ac.uk>
2024-06-02 02:36:52 +0100

    clean up, removed boilerplate code

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,8 +1,4 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import {
@@ -30,21 +26,7 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
-            <AuthButton />
-          </div>
-        </nav>
-      </div>
-
       <div className="animate-in flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
         <main className="flex-1 flex flex-col gap-6">
           <div className="space-y-4">
             <div className="flex flex-col gap-2 light p-4 bg-background border">
