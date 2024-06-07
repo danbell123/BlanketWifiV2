385ff8f7c8bddad09625c56c076938e3a85bcc9e
danbell123 <d.bell13@newcastle.ac.uk>
2024-06-07 15:30:59 +0100

    corrected gloabl styling so applied on all pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import { GeistSans } from "geist/font/sans";
 import TopNav from '@/components/topnavbar/TopNavBar';
+import './globals.css';  
 
 const Navbar = dynamic(() => import('../components/navbar/Navbar'), { ssr: false });
 
@@ -17,6 +18,8 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="flex flex-row justify-end min-h-screen bg-background text-foreground">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
         <Navbar isExpanded={isExpanded} onToggle={() => setIsExpanded(!isExpanded)} />
         <main style={{ width: isExpanded ? 'calc(100% - 250px)' : 'calc(100% - 72px)' }} className={`transition-width justify-end duration-300`}>
           <TopNav />
