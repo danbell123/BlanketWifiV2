9eb0dc945dc85b2e52251b2de7402b42717b667b
danbell123 <d.bell13@newcastle.ac.uk>
2024-06-11 22:33:25 +0100

    updated page header styling

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 type PageHeaderProps = {
   title: string;
   description?: string;
-  image?: string; // Optional image URL
+  profilePictureURL?: string;
+  customerFirstName?: string;
+  customerLastName?: string;
   backLink?: {
     url: string;
     label?: string; // Optional custom label for the back link
@@ -25,42 +28,53 @@ type PageHeaderProps = {
 export const PageHeader: React.FC<PageHeaderProps> = ({
   title,
   description,
-  image,
+  profilePictureURL,
+  customerFirstName,
+  customerLastName,
   backLink,
   primaryButton,
   secondaryButton,
 }) => {
   return (
-    <div className="flex flex-row justify-between items-center gap-8">
-        <div className="flex items-center gap-4">
-            {image && (
-                <img src={image} alt="Page Icon" className="w-12 h-12 object-cover rounded-full" />
-            )}
-            <div className='flex flex-col gap-2'>
-                {backLink && backLink.url && ( // Check if backLink.url is defined
-                    <Link href={backLink.url} legacyBehavior>
-                        <a className="text-primary hover:underline flex items-center gap-2">
-                            <span className='material-icons'>arrow_back</span>
-                            {backLink.label || 'Back'} {/* Use custom label if provided */}
-                        </a>
-                    </Link>
-                )}
-                <h1 className="text-2xl font-bold">{title}</h1>
-                {description && <p className="text-card-foreground max-w-2xl">{description}</p>}
-            </div>
-        </div>
-        <div className="flex gap-2">
-            {primaryButton && (
-            <Button onClick={primaryButton.onClick} variant={primaryButton.variant || 'default'}>
-                {primaryButton.label}
-            </Button>
-            )}
-            {secondaryButton && (
-            <Button onClick={secondaryButton.onClick} variant={secondaryButton.variant || 'secondary'}>
-                {secondaryButton.label}
-            </Button>
-            )}
-        </div>
+    <div className="flex flex-row justify-between items-end gap-8">
+          <div className="flex flex-col items-start gap-4">
+                      
+            {backLink && backLink.url && ( // Check if backLink.url is defined
+                      <Link href={backLink.url} legacyBehavior>
+                          <a className="text-primary hover:underline flex items-center gap-2">
+                              <span className='material-icons'>arrow_back</span>
+                              {backLink.label || 'Back'} {/* Use custom label if provided */}
+                          </a>
+                      </Link>
+                  )}
+
+              <div className='flex flex-row gap-2 align-middle items-center'>
+                {/*Display for customer avatar if this is a customer page*/}
+                {customerFirstName && customerLastName && 
+                    <Avatar style={{ width: '60px', height: '60px' }}>
+                        <AvatarImage src={profilePictureURL} />
+                        <AvatarFallback>{customerFirstName[0] + customerLastName[0]}</AvatarFallback>
+                    </Avatar>   
+                  }
+                  
+                  <div className='flex flex-col'>
+                    <h1 className="text-2xl font-bold">{title}</h1>
+                    {description && <p className="text-card-foreground max-w-2xl">{description}</p>}
+                  </div>
+              </div>
+          </div>
+          <div className="flex flex-row gap-2">
+              {primaryButton && (
+              <Button onClick={primaryButton.onClick} variant={primaryButton.variant || 'default'}>
+                  {primaryButton.label}
+              </Button>
+              )}
+              {secondaryButton && (
+              <Button onClick={secondaryButton.onClick} variant={secondaryButton.variant || 'secondary'}>
+                  {secondaryButton.label}
+              </Button>
+              )}
+          </div>
     </div>
   );
 };
